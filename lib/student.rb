d46b8d2c466e0cfbf77a377688cb1dd236882256
Susan Tan <ssusantan9@gmail.com>
2019-04-11 04:26:11 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -7,7 +7,7 @@ class Student
   attr_accessor :id, :name, :grade
   # attr_reader :id
   
-  def initialize(id=nil, name, grade)
+  def initialize(id= nil, name, grade)
     @id = id
     @name = name
     @grade = grade
