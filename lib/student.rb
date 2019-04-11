9d53b696c5f5c839ec905882b5a54a84eb6bd8ef
Susan Tan <ssusantan9@gmail.com>
2019-04-11 04:38:11 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -56,11 +56,10 @@ end
   end
   
   def self.new_from_db(row)
-    self.new(id, name, grade)
     id = row[0]
     name = row[1]
     grade = row[2]
-    
+    self.new(id, name, grade)
   end
     
   def update
